b3533269fea47afe015236bc0cab1b57ab6f3940
Aditya Agarwal <adi.ag321@gmail.com>
2025-11-15 16:38:52 -0600

    Update 1_Gridsearchcv.py

--- 4 - Machine Learning/14 - Hyperparameter Tunning/1_Gridsearchcv.py
+++ 4 - Machine Learning/14 - Hyperparameter Tunning/1_Gridsearchcv.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jul 16 11:23:43 2025
-
 @author: adiag
 """
-
 ## Use GridSearchCV and select the best hyperparamter for Support Vector machine
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
@@ -26,9 +21,7 @@ dataset = pd.read_csv('Advertising_data.csv')
 dataset['Gender'] = dataset['Gender'].replace({'Male': '1', 'Female': '0'})
 X = dataset.iloc[:, [1, 2, 3]].values
 y = dataset.iloc[:, 4].values
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
-
 # Feature Scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -37,10 +30,7 @@ X_test = sc.transform(X_test)
 # Fitting SVC
 classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
 y_pred = classifier.predict(X_test)
-
 cm = confusion_matrix(y_test, y_pred)
 SVC_accuracy = accuracy_score(y_test,y_pred)
 print("Accuracy of SVC without best parameters", SVC_accuracy)
@@ -67,6 +57,7 @@ print("Parameters of Grid Search CV ", grid_srch_params)
 
 ###############################
 ## Training on best params
+###############################
 classifier = SVC(kernel = 'rbf', gamma=0.7)
 classifier.fit(X_train, y_train)
 
