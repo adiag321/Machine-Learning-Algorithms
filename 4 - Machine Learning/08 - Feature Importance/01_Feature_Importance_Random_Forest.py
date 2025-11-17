7bfdac33a9c9a6ebac17b91f4a433e504af6f55c
Aditya Agarwal <adi.ag321@gmail.com>
2025-11-17 07:32:45 -0600

    Update 01_Feature_Importance_Random_Forest.py

--- 4 - Machine Learning/08 - Feature Importance/01_Feature_Importance_Random_Forest.py
+++ 4 - Machine Learning/08 - Feature Importance/01_Feature_Importance_Random_Forest.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.datasets import load_diabetes
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
