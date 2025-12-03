226ba8c14f805b2a6c5fd364c50c718d1649a7e6
Aditya Agarwal <adi.ag321@gmail.com>
2025-12-03 00:33:23 -0600

    Update 03_Feature_Agnostic_Approach_Classifier.py

--- 4 - Machine Learning/08 - Feature Importance and Model Explainability/03_Feature_Agnostic_Approach_Classifier.py
+++ 4 - Machine Learning/08 - Feature Importance and Model Explainability/03_Feature_Agnostic_Approach_Classifier.py
@@ -15,38 +15,66 @@ import time
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.inspection import permutation_importance
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
 
 project_dir = "D:/OneDrive - Northeastern University/Jupyter Notebook/Machine Learning Algorithms/4 - Machine Learning/08 - Feature Importance"
 
-###################################
-## Load the data
-###################################
+#######################################
+## Load the data and train the model
+#######################################
 # Load data
 data = load_breast_cancer()
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+df = pd.DataFrame(data.data, columns = data.feature_names)
+df['target'] = data.target
+X = df.drop(columns = 'target')
+y = df['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train Random Forest
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model.fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)     # Make predictions
+print(classification_report(y_pred, y_test))        # Classification report
 
-# Make predictions
-y_pred = model.predict(X_test)
-
-import shap
-
+#############################################
+## SHAP Explainer
+#############################################
 # Create an explainer
-explainer = shap.TreeExplainer(model)
+explainer = shap.TreeExplainer(rf_model)
 
-# Calculate SHAP values for the test set
+# Calculate SHAP values for the test set: Shap_values is in 3d array 
 shap_values = explainer(X_test)
 
-# Global Interpretation with SHAP Summary Plot
-shap.summary_plot(shap_values, X_test, feature_names=data.feature_names)
+# Summary Plot
+# Used for Global Interpretation 
+shap.summary_plot(shap_values[:,:,1], X_test, plot_type="bar")
 
+# Violin Plot
+shap.plots.violin(shap_values[:,:,1])
 
-#Local Interpretation with SHAP Waterfall Plot:
-# Generate waterfall plot for a single instance
-# Get only the positive class explanations
+# Dependence Plot
+# Its a type of scatter plot that displays how a model's predictions are affected by a specific feature
+shap.dependence_plot("mean radius", shap_values[:,:,1].values, X_test, interaction_index = "worst area")
+
+# Force Plot
+# Want to examine the first sample in the testing set to determine which features contributed to the "0" or "1" result.
+shap.plots.force(explainer.expected_value[1], shap_values[0, :, 1].values, X_test.iloc[0, :], matplotlib = True)  ## For class 1    
+shap.plots.force(explainer.expected_value[0], shap_values[0, :, 0].values, X_test.iloc[0, :], matplotlib = True)  ## For class 0
+
+# Decision Plot
+# It visually shows the model decisions by mapping the cumulative SHAP values for each prediction
+shap.decision_plot(explainer.expected_value[1], shap_values[:, :, 1].values, X_test.columns)       # For class 1
+shap.decision_plot(explainer.expected_value[0], shap_values[:, :, 0].values, X_test.columns)       # For class 0
+''' 
+Note for Decision Plot:
+Each plotted line on the decision plot shows how strongly the individual features contributed to a 
+single model prediction, thus explaining what feature values pushed the prediction.
+'''
+
+# Waterfall Plot:
+# Generate waterfall plot for a single instance (local interpretation)
 shap_values_positive = shap_values[:, :, 1]  # All samples, all features, class 1
 shap_values_negative = shap_values[:, :, 0]  # All samples, all features, class 0
 
